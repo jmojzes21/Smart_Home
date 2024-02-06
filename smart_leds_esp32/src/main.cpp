d662195ec73888cd4b7ed3747bb30b55628c0d94
Josip <jmojzes21@student.foi.hr>
2024-02-06 10:34:11 +0100

    Postavi main.cpp

--- smart_leds_esp32/src/main.cpp
+++ smart_leds_esp32/src/main.cpp
@@ -1,18 +1,13 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Serial.println("Smart LEDs");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
