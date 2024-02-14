a51c82dd88a843d98affe06da27bf70b41f58d6f
Josip <jmojzes21@student.foi.hr>
2024-02-14 18:30:56 +0100

    Postavi tools projekt

--- smart_leds_tools/bin/smart_leds_tools.dart
+++
@@ -1,5 +0,0 @@
-import 'package:smart_leds_tools/smart_leds_tools.dart' as smart_leds_tools;
-
-void main(List<String> arguments) {
-  print('Hello world: ${smart_leds_tools.calculate()}!');
-}
--- smart_leds_tools/test/smart_leds_tools_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:smart_leds_tools/smart_leds_tools.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
