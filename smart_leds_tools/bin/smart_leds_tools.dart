5d523916cd0930de0e383f2117285f5449192649
Josip <jmojzes21@student.foi.hr>
2024-02-14 18:26:37 +0100

    Kreiraj tools projekt

---
+++ smart_leds_tools/bin/smart_leds_tools.dart
@@ -0,0 +1,5 @@
+import 'package:smart_leds_tools/smart_leds_tools.dart' as smart_leds_tools;
+
+void main(List<String> arguments) {
+  print('Hello world: ${smart_leds_tools.calculate()}!');
+}
---
+++ smart_leds_tools/test/smart_leds_tools_test.dart
@@ -0,0 +1,8 @@
+import 'package:smart_leds_tools/smart_leds_tools.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
