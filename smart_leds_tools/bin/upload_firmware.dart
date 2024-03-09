591cfd5580cce0a7c6b8eb36ccd1159f9aabd09f
Josip <jmojzes21@student.foi.hr>
2024-03-09 10:38:06 +0100

    Završi skriptu za prijenos programa

--- smart_leds_tools/bin/upload_firmware.dart
+++ smart_leds_tools/bin/upload_firmware.dart
@@ -1,12 +1,57 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:smart_leds_tools/firmware.dart';
 
 Future<void> main(List<String> args) async {
   var ipAddress = readIpAddress(args);
   print('Prenesi ugradbeni program na uređaj ${ipAddress.address}');
 
-  print('Gotovo');
+  Stopwatch stopwatch = Stopwatch();
+  stopwatch.start();
+
+  Firmware firmware = loadFirmware();
+  await checkDeviceType(ipAddress, firmware.deviceType);
+  await uploadFirmware(ipAddress, firmware);
+
+  stopwatch.stop();
+  double elapsed = stopwatch.elapsedMilliseconds / 1000;
+
+  print('Gotovo, trajanje: ${elapsed.toStringAsFixed(2)} s');
+}
+
+Future<void> checkDeviceType(InternetAddress ipAddress, String type) async {
+  var uri = Uri.http(ipAddress.address, 'device');
+  var response = await http.get(uri);
+
+  var json = jsonDecode(response.body);
+  if (json['type'] != type) {
+    throw 'Pogrešan uređaj';
+  }
+}
+
+Future<void> uploadFirmware(
+    InternetAddress ipAddress, Firmware firmware) async {
+  var uri = Uri.http(ipAddress.address, 'firmware_update');
+  var response = await http.post(
+    uri,
+    headers: {
+      'Device-Type': firmware.deviceType,
+      'Firmware-Size': firmware.bytes.length.toString(),
+      'Firmware-HMAC': firmware.hmac,
+    },
+    body: firmware.bytes,
+  );
+
+  var json = jsonDecode(response.body);
+  var msg = json['msg'];
+
+  if (response.statusCode != 201) {
+    throw Exception('${response.statusCode} $msg');
+  }
+
+  print('${response.statusCode} $msg');
 }
 
 InternetAddress readIpAddress(List<String> args) {
