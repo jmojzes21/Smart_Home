3b04fb89365fdf0b14d8582f4a47e8693eefa1dc
Josip <jmojzes21@student.foi.hr>
2024-03-09 16:21:40 +0100

    Update exceptions

--- smart_leds_app/lib/models/exceptions.dart
+++ smart_leds_app/lib/models/exceptions.dart
@@ -1,5 +1,23 @@
-class DeviceException implements Exception {
+class AppException implements Exception {
   final String message;
+  AppException(this.message);
 
-  DeviceException(this.message);
+  @override
+  String toString() => message;
+}
+
+class DeviceException extends AppException {
+  DeviceException(super.message);
+}
+
+class FirmwareException extends AppException {
+  FirmwareException(super.message);
+}
+
+class InvalidFirmwareFileException extends FirmwareException {
+  InvalidFirmwareFileException() : super('Datoteka programa nije valjana.');
+}
+
+class FirmwareUpdateException extends AppException {
+  FirmwareUpdateException(super.message);
 }
