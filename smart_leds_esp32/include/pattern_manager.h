329fb776f2980e6eac68f74ce21cc7a747bbf43a
Josip <jmojzes21@student.foi.hr>
2024-03-16 20:10:41 +0100

    Dodaj izravni pristup, koristi mutex

--- smart_leds_esp32/include/pattern_manager.h
+++ smart_leds_esp32/include/pattern_manager.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <AsyncUDP.h>
+
 #include "patterns/base_pattern.h"
 
 #define LED_COUNT 7
@@ -8,19 +10,40 @@
 
 class PatternManager {
 
-    public:
+    private:
 
-    const int ledCount = LED_COUNT;
-    CRGB leds[LED_COUNT];
-    BasePattern* currentPattern = nullptr;
+    SemaphoreHandle_t _mutex;
+
+    uint8_t _updateCode = 0;
+    JsonDocument _patternJson;
+    uint8_t _brightness;
+
+    bool _directAccess = false;
+    AsyncUDP _udp;
+
+    const int _ledCount = LED_COUNT;
+    CRGB _leds[LED_COUNT];
+    BasePattern* _currentPattern = nullptr;
+
+    public:
 
     void setup();
     void loop();
 
-    bool changePattern(JsonObject& pattern);
-    bool updatePattern(JsonObject& pattern);
+    void updatePattern(JsonVariant& json);
+    void setBrightness(int value);
+
+    void enableDirectAccess();
 
     private:
+
+    void _handleUpdate();
+
+    void _changePattern();
+    void _updatePattern();
+
+    void _enableDirectAccess();
+    void _onUdpPacket(AsyncUDPPacket& packet);
 
     BasePattern* _createPattern(std::string& name);
 
--- smart_leds_esp32/src/pattern_manager.cpp
+++ smart_leds_esp32/src/pattern_manager.cpp
@@ -5,63 +5,189 @@
 #include "patterns/solid_color.h"
 #include "patterns/fade_pattern.h"
 
+#include "log.h"
+
+#define UPDATE_CODE_DIRECT_ACCESS 1
+#define UPDATE_CODE_PATTERN 2
+#define UPDATE_CODE_BRIGHTNESS 3
+#define UPDATE_CODE_ENABLE_DIRECT_ACCESS 4
+
+#define LED_DATA_PAYLOAD_SIZE (LED_COUNT * 3)
+
 void PatternManager::setup() {
 
-    FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(leds, LED_COUNT);
+    _mutex = xSemaphoreCreateMutex();
+
+    FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(_leds, LED_COUNT);
     FastLED.clear(true);
     
-    FastLED.setBrightness(40);
+    FastLED.setBrightness(80);
+
+    _udp.onPacket([&](AsyncUDPPacket packet) {
+        _onUdpPacket(packet);
+    });
 
 }
 
 void PatternManager::loop() {
 
-    if(currentPattern != nullptr) {
-        currentPattern->loop();
+    if(xSemaphoreTake(_mutex, portMAX_DELAY) == pdTRUE) {
+
+        if(_updateCode != 0) {
+            _handleUpdate();
+        }
+
+        if(_currentPattern != nullptr) {
+            _currentPattern->loop();
+        }
+        
+        xSemaphoreGive(_mutex);
     }
 
 }
 
-bool PatternManager::changePattern(JsonObject& pattern) {
+void PatternManager::updatePattern(JsonVariant& json) {
 
-    std::string name = pattern["name"];
-    if(name == "") return false;
+    if(xSemaphoreTake(_mutex, portMAX_DELAY) == pdTRUE) {
+        
+        if(_directAccess == false) {
+            _updateCode = UPDATE_CODE_PATTERN;
+            _patternJson = json;
+        }
+        
+        xSemaphoreGive(_mutex);
+    }
+
+}
+
+void PatternManager::setBrightness(int value) {
+
+    if(xSemaphoreTake(_mutex, portMAX_DELAY) == pdTRUE) {
+
+        if(_directAccess == false) {
+            _updateCode = UPDATE_CODE_BRIGHTNESS;
+            _brightness = value;
+        }
+        
+        xSemaphoreGive(_mutex);
+    }
+
+}
+
+void PatternManager::enableDirectAccess() {
+
+    if(xSemaphoreTake(_mutex, portMAX_DELAY) == pdTRUE) {
+
+        if(_directAccess == false) {
+            _updateCode = UPDATE_CODE_ENABLE_DIRECT_ACCESS;
+        }
+        
+        xSemaphoreGive(_mutex);
+    }
+
+}
+
+void PatternManager::_handleUpdate() {
+
+    switch (_updateCode) {
+
+        case UPDATE_CODE_DIRECT_ACCESS:
+            FastLED.show();
+            break;
+
+        case UPDATE_CODE_PATTERN:
+            if(_patternJson.containsKey("name")) {
+                _changePattern();
+            }else{
+                _updatePattern();
+            }
+            _patternJson.clear();
+
+            break;
+
+        case UPDATE_CODE_BRIGHTNESS:
+            if(_brightness > 80) _brightness = 80;
+            FastLED.setBrightness(_brightness);
+            _brightness = 0;
+            break;
+
+        case UPDATE_CODE_ENABLE_DIRECT_ACCESS:
+            _enableDirectAccess();
+            break;
+
+    }
+
+    _updateCode = 0;
+
+}
+
+void PatternManager::_changePattern() {
+
+    JsonObject p = _patternJson.as<JsonObject>();
+    std::string name = p["name"];
 
     BasePattern* newPattern = _createPattern(name);
-    if(newPattern == nullptr) return false;
+    if(newPattern == nullptr) return;
 
     newPattern->setup();
-    bool result = newPattern->update(pattern);
+    newPattern->update(p);
 
-    if(result == false) {
-        newPattern->dispose();
-        delete newPattern;
-        return false;
+    if(_currentPattern != nullptr) {
+        _currentPattern->dispose();
+        delete _currentPattern;
     }
 
-    if(currentPattern != nullptr) {
-        currentPattern->dispose();
-        delete currentPattern;
-    }
-
-    currentPattern = newPattern;
-    return true;
+    _currentPattern = newPattern;
     
 }
 
-bool PatternManager::updatePattern(JsonObject& pattern) {
+void PatternManager::_updatePattern() {
     
-    if(currentPattern == nullptr) return false;
+    if(_currentPattern == nullptr);
 
-    return currentPattern->update(pattern);
+    JsonObject p = _patternJson.as<JsonObject>();
+    _currentPattern->update(p);
+
+}
+
+void PatternManager::_enableDirectAccess() {
+
+    if(_currentPattern != nullptr) {
+        _currentPattern->dispose();
+        delete _currentPattern;
+        _currentPattern = nullptr;
+    }
+
+    FastLED.clear(true);
+
+    _udp.listen(7000);
+    _directAccess = true;
+
+}
+
+void PatternManager::_onUdpPacket(AsyncUDPPacket& packet) {
+
+    uint8_t* data = packet.data();
+    int length = packet.length();
+
+    if(xSemaphoreTake(_mutex, portMAX_DELAY) == pdTRUE) {
+        
+        if(_directAccess && length >= sizeof(_leds)) {
+            memcpy(_leds, data, sizeof(_leds));
+            _updateCode = UPDATE_CODE_DIRECT_ACCESS;
+        }
+
+        xSemaphoreGive(_mutex);
+    }
+
 }
 
 BasePattern* PatternManager::_createPattern(std::string& name) {
 
     if(name == "solid") {
-        return new SolidColorPattern(leds, ledCount);
+        return new SolidColorPattern(_leds, _ledCount);
     }else if(name == "fade") {
-        return new FadePattern(leds, ledCount);
+        return new FadePattern(_leds, _ledCount);
     }
 
     return nullptr;
