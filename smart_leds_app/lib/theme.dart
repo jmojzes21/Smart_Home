d9bdb02df7d7a02042e8c68273de64dbbb443f7c
Josip <jmojzes21@student.foi.hr>
2024-04-18 11:38:41 +0200

    Napravi temu

---
+++ smart_leds_app/lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  //
+  static const TextStyle titleLarge = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle titleMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 16,
+  );
+  //
+}
