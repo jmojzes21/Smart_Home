3f9904b8a2f12a017489e9aaab83607dcb0d04fa
Josip <jmojzes21@student.foi.hr>
2024-05-04 18:58:08 +0200

    Dodaj dijalog za dodatnu potvrdu

--- smart_leds_app/lib/widgets/dialogs/simple_dialogs.dart
+++ smart_leds_app/lib/widgets/dialogs/simple_dialogs.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:smart_leds_app/widgets/misc/checkbox.dart';
 
 class SimpleDialogs {
-  static void showMessage(
+  static Future<void> showMessage(
       {required BuildContext context,
       required String title,
       required String message}) {
-    showDialog(
+    return showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: Text(title),
@@ -43,5 +44,71 @@ class SimpleDialogs {
     );
 
     return result ?? false;
+  }
+
+  static Future<bool> showExtraConfirm({
+    required BuildContext context,
+    required String title,
+    required String message,
+    required String checkboxText,
+  }) async {
+    var result = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return _ExtraConfirmDialog(title, message, checkboxText);
+      },
+    );
+
+    return result ?? false;
+  }
+}
+
+class _ExtraConfirmDialog extends StatefulWidget {
+  final String title;
+  final String message;
+  final String checkboxText;
+
+  const _ExtraConfirmDialog(this.title, this.message, this.checkboxText);
+  @override
+  State<_ExtraConfirmDialog> createState() => _ExtraConfirmDialogState();
+}
+
+class _ExtraConfirmDialogState extends State<_ExtraConfirmDialog> {
+  bool isChecked = false;
+
+  void confirm() {
+    if (isChecked) {
+      Navigator.of(context).pop(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(widget.title),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(widget.message),
+          SizedBox(height: 10),
+          CheckboxText(
+            value: isChecked,
+            text: widget.checkboxText,
+            onChanged: (value) => setState(() => isChecked = value),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: Text('Ne'),
+        ),
+        TextButton(
+          onPressed: isChecked ? confirm : null,
+          child: Text('Da'),
+        ),
+      ],
+    );
   }
 }
