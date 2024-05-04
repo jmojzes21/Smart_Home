6b2353b8ddecd768292952c5485f719ebd46b3d8
Josip <jmojzes21@student.foi.hr>
2024-05-04 12:00:26 +0200

    Wifi upravitelj

--- smart_leds_app/lib/logic/device/wifi_controller.dart
+++ smart_leds_app/lib/logic/device/wifi_controller.dart
@@ -2,12 +2,22 @@ import 'package:smart_leds_app/logic/device/device.dart';
 import 'package:smart_leds_app/models/misc/wifi_network.dart';
 
 class WifiController {
-  // ignore: unused_field
   final Device _device;
 
   WifiController(this._device);
 
-  Future<List<WifiNetwork>> getNetworks() async => [];
+  Future<List<WifiNetwork>> getNetworks() async {
+    var json = await _device.getHttp(path: '/wifi_networks');
+    var networks = json['networks'] as List<dynamic>;
 
-  Future<void> updateNetworks(List<WifiNetwork> networks) async {}
+    return networks.map((e) {
+      return WifiNetwork.fromJson(e);
+    }).toList();
+  }
+
+  Future<void> updateNetworks(List<WifiNetwork> networks) async {
+    await _device.postHttp(path: '/wifi_networks', body: {
+      'networks': networks.map((e) => WifiNetwork.toJson(e)).toList(),
+    });
+  }
 }
--- smart_leds_app/lib/models/misc/wifi_network.dart
+++ smart_leds_app/lib/models/misc/wifi_network.dart
@@ -3,4 +3,15 @@ class WifiNetwork {
   String password = '';
 
   WifiNetwork(this.ssid, this.password);
+
+  factory WifiNetwork.fromJson(Map<String, dynamic> src) {
+    return WifiNetwork(src['ssid'], src['pass']);
+  }
+
+  static Map<String, dynamic> toJson(WifiNetwork network) {
+    return {
+      'ssid': network.ssid,
+      'pass': network.password,
+    };
+  }
 }
