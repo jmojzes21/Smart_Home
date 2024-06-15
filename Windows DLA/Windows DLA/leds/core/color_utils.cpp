31db87a0b6df698b97de91b75b2147cae34530a1
Josip <jmojzes21@student.foi.hr>
2024-06-15 12:25:45 +0200

    Update basic colors esp

--- Windows DLA/Windows DLA/leds/core/color_utils.cpp
+++ Windows DLA/Windows DLA/leds/core/color_utils.cpp
@@ -46,24 +46,16 @@ namespace utils {
     }
 
     void getBasicColors(std::vector<Color>& colors) {
-        colors.emplace_back(244, 67, 54);
-        colors.emplace_back(233, 30, 99);
-        colors.emplace_back(156, 39, 176);
-        colors.emplace_back(103, 58, 183);
-        colors.emplace_back(63, 81, 181);
-        colors.emplace_back(33, 150, 243);
-        colors.emplace_back(3, 169, 244);
-        colors.emplace_back(0, 188, 212);
-        colors.emplace_back(0, 150, 136);
-        colors.emplace_back(76, 175, 80);
-        colors.emplace_back(139, 195, 74);
-        colors.emplace_back(205, 220, 57);
-        colors.emplace_back(255, 235, 59);
-        colors.emplace_back(255, 193, 7);
-        colors.emplace_back(255, 152, 0);
-        colors.emplace_back(255, 87, 34);
-        colors.emplace_back(121, 85, 72);
-        colors.emplace_back(96, 125, 139);
+        colors.emplace_back(255, 0, 0);
+        colors.emplace_back(255, 94, 0);
+        colors.emplace_back(255, 255, 0);
+        colors.emplace_back(127, 255, 0);
+        colors.emplace_back(0, 255, 0);
+        colors.emplace_back(0, 191, 255);
+        colors.emplace_back(0, 0, 255);
+        colors.emplace_back(255, 0, 255);
+        colors.emplace_back(127, 0, 255);
+        colors.emplace_back(255, 0, 64);
     }
 
     Color scaleColor(Color color, float scale) {
