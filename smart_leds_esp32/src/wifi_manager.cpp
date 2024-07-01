839a730a9d0913a14c5da038d9426f32486c3251
jmojzes21 <jmojzes21@student.foi.hr>
2024-07-01 20:22:30 +0200

    Dodaj komentare

--- smart_leds_esp32/src/wifi_manager.cpp
+++ smart_leds_esp32/src/wifi_manager.cpp
@@ -5,15 +5,21 @@ static const int wifiConnectTimeout = 4000;
 
 extern Device device;
 
+/// @brief Poveži se na wifi mrežu
+/// ili otvori wifi pristupnu točku.
 void WifiManager::connectToWifi() {
 
+    // Učitaj postavke
     Settings settings;
     settings.load();
 
+    // Lista wifi mreža
     auto& networks = settings.wifiNetworks;
+
+    // Indeks preferirane mreže ili -1
     int preferred = settings.preferredWifiNetwork;
 
-    // ako nema wifi mreža,
+    // Ako nema zapisanih wifi mreža,
     // otvori wifi pristupnu točku
     if(networks.size() == 0) {
         Serial.printf("Nema wifi mreža za povezivanje\n");
@@ -21,49 +27,53 @@ void WifiManager::connectToWifi() {
         return;
     }
 
-    // ako imamo poželjnu mrežu
-    // pokušajmo se povezati na nju
+    // Ako imamo preferiranu mrežu,
+    // pokušaj se povezati na nju
     if(preferred >= 0 && preferred < networks.size()) {
         
         WifiNetwork& network = networks[preferred];
         bool success = _connectToNetwork(network);
 
         if(success) {
-            // uspjeli smo se povezati na poželjnu mrežu
+            // Uspješno povezivanje na preferiranu mrežu
             return;
         }
     }
 
-    // nema poželjne mreže ili se nismo
+    // Nema preferirane mreže ili se nismo
     // uspjeli povezati na nju
     
-    // pretraži dostupne wifi mreže i
+    // Pretraži dostupne wifi mreže i
     // poveži se na najbolju
 
     int best = _findBestNetwork(networks);
 
     if(best == -1 || best == preferred) {
-        // nema dostupnih wifi mreža na koje se možemo povezati
+        // Nema dostupnih wifi mreža na koje se možemo povezati
         Serial.printf("Nema dostupnih wifi mreža za povezivanje\n");
         _startAccessPoint();
         return;
     }
 
-    // poveži se na najbolju mrežu
+    // Poveži se na najbolju mrežu
 
     WifiNetwork& bestNetwork = networks[best];
     bool success = _connectToNetwork(bestNetwork);
 
     if(success) {
-        // postavi najbolju mrežu kao preferiranu
+        // Postavi najbolju mrežu kao preferiranu
         settings.preferredWifiNetwork = best;
         settings.save();
     }else{
+        // Povezivanje na najbolju mrežu nije moguće
         _startAccessPoint();
     }
 
 }
 
+/// @brief Pokušaj se povezati na wifi mrežu.
+/// @param network wifi mreža
+/// @return true ako se uspije poveziti, inače false.
 bool WifiManager::_connectToNetwork(WifiNetwork& network) {
 
     Serial.printf("Poveži se na wifi mrežu %s\n", network.ssid.c_str());
@@ -77,7 +87,7 @@ bool WifiManager::_connectToNetwork(WifiNetwork& network) {
 
         uint32_t now = millis();
         if(now - start > wifiConnectTimeout) {
-            // nismo se uspjeli povezati na wifi mrežu
+            // Nismo se uspjeli povezati na wifi mrežu
             // unutar određenog perioda
             Serial.printf("Povezivanje na mrežu nije uspjelo\n");
             return false;
@@ -86,23 +96,27 @@ bool WifiManager::_connectToNetwork(WifiNetwork& network) {
         delay(500);
     }
 
-    // povezali smo se na wifi mrežu
+    // Povezali smo se na wifi mrežu
     Serial.printf("Povezano\n");
     Serial.printf("  IP: %s\n", WiFi.localIP().toString().c_str());
 
     return true;
 }
 
+/// @brief Pretraži dostupne wifi mreže.
+/// Zatim, pronađi wifi mrežu s najboljim signalom za koju znamo lozinku.
+/// @param networks mreže na koje se možemo povezati jer znamo lozinku
+/// @return indeks najbolje mreže ili -1
 int WifiManager::_findBestNetwork(std::vector<WifiNetwork>& networks) {
 
-    // pretraži dostupne wifi mreže
+    // Pretraži dostupne wifi mreže
     Serial.printf("Pretraži dostupne wifi mreže\n");
 
     WiFi.disconnect();
     WiFi.mode(WIFI_STA);
     int netCount = WiFi.scanNetworks();
 
-    // pronađi najbolju mrežu na koju
+    // Pronađi najbolju mrežu na koju
     // se uređaj može povezati
 
     int bestIndex = -1;
@@ -117,7 +131,7 @@ int WifiManager::_findBestNetwork(std::vector<WifiNetwork>& networks) {
 
         Serial.printf("  %s [%d]\n", ssid.c_str(), rssi);
 
-        // možemo li se povezati na mrežu (ssid)
+        // Možemo li se povezati na mrežu (ssid),
         // tj. imamo li konfiguraciju za nju
 
         int ni = -1;
@@ -130,7 +144,7 @@ int WifiManager::_findBestNetwork(std::vector<WifiNetwork>& networks) {
         }
 
         if(ni == -1) {
-            // ne možemo se povezati na tu mrežu
+            // Ne možemo se povezati na tu mrežu
             continue;
         }
 
@@ -145,6 +159,8 @@ int WifiManager::_findBestNetwork(std::vector<WifiNetwork>& networks) {
     return bestIndex;
 }
 
+/// @brief Pokreni wifi pristupnu točku.
+/// Naziv mreže je jednak nazivu uređaja, a lozinke nema.
 void WifiManager::_startAccessPoint() {
 
     WiFi.disconnect();
