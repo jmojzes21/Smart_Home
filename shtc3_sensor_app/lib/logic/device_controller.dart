175c9505fb3b79b0bb11675c359ca09826f9cd16
Josip Mojzeš <jmojzes21@student.foi.hr>
2025-07-07 14:22:42 +0200

    Remove ble

--- shtc3_sensor_app/lib/logic/device_controller.dart
+++ shtc3_sensor_app/lib/logic/device_controller.dart
@@ -1,15 +1,1 @@
-import 'dart:async';
-
-import 'package:shtc3_sensor_app/models/sensor_data.dart';
-
-abstract class DeviceController {
-  final String _name;
-
-  DeviceController(this._name);
-
-  Future<void> connect();
-  Future<void> disconnect();
-
-  Stream<SensorData> get sensorDataStream;
-  String get name => _name;
-}
+class DeviceController {}
--- shtc3_sensor_app/lib/pages/device_page.dart
+++ shtc3_sensor_app/lib/pages/device_page.dart
@@ -1,58 +1,15 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:shtc3_sensor_app/logic/device_controller.dart';
-import 'package:shtc3_sensor_app/models/sensor_data.dart';
 
 class DevicePage extends StatefulWidget {
-  final DeviceController deviceController;
-
-  const DevicePage(this.deviceController, {super.key});
+  const DevicePage({super.key});
 
   @override
   State<DevicePage> createState() => _DevicePageState();
 }
 
 class _DevicePageState extends State<DevicePage> {
-  late DeviceController deviceController;
-
-  StreamSubscription<SensorData>? sensorDataSubscription;
-  SensorData sensorData = SensorData(temperature: 0, humidity: 0);
-
-  @override
-  void initState() {
-    super.initState();
-    deviceController = widget.deviceController;
-    sensorDataSubscription = deviceController.sensorDataStream.listen(onSensorData);
-  }
-
-  void onSensorData(SensorData sensorData) {
-    setState(() {
-      this.sensorData = sensorData;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('SHTC3 senzor temperature')),
-      body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Temperatura: ${sensorData.temperature.toStringAsFixed(2)} °C'),
-            Text('Vlažnost: ${sensorData.humidity.round()} %'),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    sensorDataSubscription?.cancel();
-    deviceController.disconnect();
-    super.dispose();
+    return Scaffold();
   }
 }
