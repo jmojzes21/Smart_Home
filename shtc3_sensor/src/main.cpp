91bc94ca5473dd52ac9f3c4bb0850f5549fa6255
Josip Mojzeš <jmojzes21@student.foi.hr>
2025-07-07 11:06:32 +0200

    Remove ble

--- shtc3_sensor/src/main.cpp
+++ shtc3_sensor/src/main.cpp
@@ -2,25 +2,13 @@
 #include <Arduino.h>
 #include <SHTC3-SOLDERED.h>
 
-#include "ble_controller.h"
-
 SHTC3 shtc3Sensor;
-BleController bleController;
 
 void setup() {
   shtc3Sensor.begin();
-  bleController.init();
+
 }
 
 void loop() {
-
-  shtc3Sensor.sample();
-
-  float temperature = shtc3Sensor.readTempC();
-  float humidity = shtc3Sensor.readHumidity();
-
-  bleController.sendSensorData(temperature, humidity);
-
-  delay(2000);
 
 }
