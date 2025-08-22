c58092c74d14ada899ae7dbe9a48d95ce190900c
Josip Mojzeš <jmojzes21@student.foi.hr>
2025-08-22 10:17:54 +0200

    Create exception page

---
+++ air_quality_app/lib/pages/exception_page.dart
@@ -0,0 +1,32 @@
+import 'package:air_quality_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class ExceptionPage extends StatelessWidget {
+  final String message;
+
+  const ExceptionPage(this.message, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dogodila se greška')),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(message),
+            SizedBox(height: 20),
+            FilledButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              child: Text('Početna stranica'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
