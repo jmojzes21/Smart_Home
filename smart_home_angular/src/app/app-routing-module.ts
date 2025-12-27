e883ce04032d1856ca7cfc45a74d4e27ea27f1da
Josip Mojzeš <jmojzes21@student.foi.hr>
2025-12-27 16:13:58 +0100

    Update app module

--- smart_home_angular/src/app/app-routing-module.ts
+++ smart_home_angular/src/app/app-routing-module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicesPage } from './devices/pages/devices-page/devices-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'devices', pathMatch: 'full' },
+  {
+    path: 'devices',
+    component: DevicesPage,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
