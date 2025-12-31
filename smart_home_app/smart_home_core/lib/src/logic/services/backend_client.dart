3d65a6481a09697e1a662a3c5c6e5d7802e5b598
Josip Mojzeš <jmojzes21@student.foi.hr>
2025-12-31 19:48:29 +0100

    Update backend client

--- smart_home_app/smart_home_core/lib/src/logic/services/backend_client.dart
+++ smart_home_app/smart_home_core/lib/src/logic/services/backend_client.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 
@@ -9,38 +10,30 @@ class BackendClient {
   final Duration _timeout = Duration(seconds: 10);
 
   Future<dynamic> httpGet(String path, [Map<String, dynamic>? queryParameters]) async {
-    try {
-      var hostname = AppContext.instance.backendHostname;
-      var url = Uri.http(hostname, path, queryParameters);
-      var response = await http.get(url).timeout(_timeout);
+    var hostname = AppContext.instance.backendHostname;
+    var url = Uri.http(hostname, path, queryParameters);
+    var response = await http.get(url).timeout(_timeout);
 
-      if (response.statusCode != 200) {
-        throw AppException(response.body);
-      }
-
-      return jsonDecode(response.body);
-    } on Exception catch (e) {
-      throw AppException('${e.runtimeType} $e');
+    if (response.statusCode != 200) {
+      throw AppException(response.body);
     }
+
+    return jsonDecode(response.body);
   }
 
   Future<dynamic> httpPost(String path, Object body) async {
+    var headers = {'Content-Type': 'application/json'};
     String bodyJson = jsonEncode(body);
 
-    try {
-      var headers = {'Content-Type': 'application/json'};
+    var hostname = AppContext.instance.backendHostname;
+    var url = Uri.http(hostname, path);
 
-      var hostname = AppContext.instance.backendHostname;
-      var url = Uri.http(hostname, path);
-      var response = await http.post(url, headers: headers, body: bodyJson, encoding: utf8).timeout(_timeout);
+    var response = await http.post(url, headers: headers, body: bodyJson, encoding: utf8).timeout(_timeout);
 
-      if (response.statusCode != 200) {
-        throw AppException(response.body);
-      }
-
-      return jsonDecode(response.body);
-    } on Exception catch (e) {
-      throw AppException('${e.runtimeType} $e');
+    if (response.statusCode != 200) {
+      throw AppException(response.body);
     }
+
+    return jsonDecode(response.body);
   }
 }
